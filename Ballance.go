2bb36908e2bc4c2170490443854484dab6e99c0e
Jordan <40551381+Meschdog18@users.noreply.github.com>
2019-07-10 16:38:27 +0100

    Add files via upload

--- Ballance.go
+++ Ballance.go
@@ -6,7 +6,6 @@ import(
 	"strconv"
 	"bufio"
 	"os"
-//io/ioutil"
 	
 )
 func main(){
@@ -14,13 +13,14 @@ numi := 0
 // n := 0
 holdi := 0
 var numbers []int
-
+//Opens Input File
 file, _:=os.Open("input.txt")
 scanner := bufio.NewScanner(file)
 for scanner.Scan(){
 	line := scanner.Text()
+    converts string from txt file to ints 
     lineint, _:= strconv.Atoi(line) 
-
+    //adds each line of numbers to the array
 	numbers = append(numbers, lineint)
 }
 
@@ -29,9 +29,6 @@ for scanner.Scan(){
 
 
 
-//numbers, _= strconv.Atoi(data) 
-
-//numbers = append(numbers,data)
 
 
 fmt.Println(len(numbers))
@@ -40,9 +37,11 @@ for i := range(numbers) {
  	
 
 for tru:=0;tru == 0;{
+	//trys to find number without 4 by subtracting its value by numi(which increase by one each loop)
 	holdi = numbers[i] - numi
  	hold:= strconv.Itoa(holdi)
  	num:= strconv.Itoa(numi)
+ 	//if both number a and b, dont have a 4 in them than it resets the loop and goes to tthe next number in the input file
  	if(strings.Contains(hold, "4") ==  false && strings.Contains(num, "4") == false){
 
  		tru = 1;
